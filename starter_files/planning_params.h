80648dae5046a9755dede6d925697067b229d157
RuchitJathania <44512765+RuchitJathania@users.noreply.github.com>
2022-08-29 14:39:13 -0400

    Made P_NUM_PATHS an odd value

--- starter_files/planning_params.h
+++ starter_files/planning_params.h
@@ -13,7 +13,7 @@
 #include <array>
 
 // Planning Constants
-#define P_NUM_PATHS 6                  // TODO - Num of paths (goals)
+#define P_NUM_PATHS 7                  // TODO - Num of paths (goals)
 #define P_LOOKAHEAD_MIN 8.0            // m
 #define P_LOOKAHEAD_MAX 20.0           // m
 #define P_LOOKAHEAD_TIME 1.5           // s
